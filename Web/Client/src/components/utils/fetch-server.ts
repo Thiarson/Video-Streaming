6eae50aff766c189e001d5f6e7cd9cb68920d5e6
Thiarson <thiarsonantsa@gmail.com>
2024-05-28 20:21:54 +0300

    Fix bug in fetch

--- Web/Client/src/components/utils/fetch-server.ts
+++ Web/Client/src/components/utils/fetch-server.ts
@@ -19,48 +19,42 @@ const defaultOptions = {
   },
 }
 
-const fetchWithTimeout = (resource: any, timeout: number = 6000) => {
-  const { url, headers, body } = resource
-
-  return new Promise((resolve, reject) => {
-    const controller = new AbortController()
-    const id = setTimeout(() => {
-      controller.abort()
-      reject(new Error("Request timed out"))
-    }, timeout);
-    const signal = controller.signal
-
-    fetch(`${baseURL}${url}`, {
-      method: "POST",
-      headers: {
-        ...defaultOptions.headers,
-        ...headers,
-      },
-      body: JSON.stringify(body),
-      signal: signal,
-    })
-      .then((response) => {
-        clearTimeout(id)
-        if (!response.ok)
-          throw new Error("Fetch response was not ok")
-
-        resolve(response.json())
-      })
-      .catch((error) => {
-        clearTimeout(id)
-        reject(error)
-      })
-  })
-}
-
 const fetchServer = {
   get: null,
   post: async (url: string, options: FetchOptions = {}) => {
     const headers = { ...options.headers }
     const body = { ...options.body }
-    const response = await fetchWithTimeout({ url, headers, body })
-    
-    return response
+
+    return new Promise((resolve, reject) => {
+      const controller = new AbortController()
+      const timeout = 6000
+      const id = setTimeout(() => {
+        controller.abort()
+        reject(new Error("Request timed out"))
+      }, timeout);
+      const signal = controller.signal
+  
+      fetch(`${baseURL}${url}`, {
+        method: "POST",
+        headers: {
+          ...defaultOptions.headers,
+          ...headers,
+        },
+        body: JSON.stringify(body),
+        signal: signal,
+      })
+        .then((response) => {
+          clearTimeout(id)
+          if (!response.ok)
+            throw new Error("Fetch response was not ok")
+  
+          resolve(response.json())
+        })
+        .catch((error) => {
+          clearTimeout(id)
+          reject(error)
+        })
+    })
   }
 }
 
